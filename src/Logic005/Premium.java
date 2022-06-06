3b941f6e5e1cd68bd70bf30c90a112a465294a57
EgorKorolevskiy <korollevskiy@gmail.com>
2022-06-06 23:53:52 +0300

    Прибавка к ЗП V.3.0

--- src/Logic005/Premium.java
+++ src/Logic005/Premium.java
@@ -15,17 +15,17 @@ public class Premium {
         int userZP = cns.nextInt(); // какая зп сейчас
 
         if (userVisluga >= 0 && userVisluga <= 4) {
-            System.out.println("Братан, ток 15% накинем, чисто на проезд. Итого: " + (userZP/100) * 10 + " к зп.");
+            System.out.println("Братан, ток 10% накинем, чисто на проезд. Итого: " + (userZP/100) * 10 + " к зп.");
         } else if (userVisluga >= 5 && userVisluga <= 9) {
-            System.out.println("Нормуль поднялся. Скок работал, сток получай - 25% прибавку. Итого: " + (userZP/100) * 15 + " к зп.");
+            System.out.println("Нормуль поднялся. Скок работал, сток получай - 15% прибавку. Итого: " + (userZP/100) * 15 + " к зп.");
         } else if (userVisluga >= 10 && userVisluga <= 14) {
-            System.out.println("Кошерно брат, тебе 35% к зп. Итого: " + (userZP/100) * 25 + " к зп.");
+            System.out.println("Кошерно брат, тебе 25% к зп. Итого: " + (userZP/100) * 25 + " к зп.");
         } else if (userVisluga >= 15 && userVisluga <= 19) {
-            System.out.println("Даже я столько не работал, смотри не рипнись. Тебе + 45%. Итого: " + (userZP/100) * 35 + " к зп.");
+            System.out.println("Даже я столько не работал, смотри не рипнись. Тебе + 35%. Итого: " + (userZP/100) * 35 + " к зп.");
         } else if (userVisluga >= 20 && userVisluga <= 24) {
-            System.out.println("Тебе пора на пенсию, бро, подумай о внуках. Вот тебе + 50%. Итого: " + (userZP/100) * 45 + " к зп.");
+            System.out.println("Тебе пора на пенсию, бро, подумай о внуках. Вот тебе + 45%. Итого: " + (userZP/100) * 45 + " к зп.");
         } else if (userVisluga >= 25) {
-            System.out.println("Это ГГ бро. Итого: " + (userZP/100) * 50 + " к зп.");
+            System.out.println("Это ГГ бро. 50% тебе. Итого: " + (userZP/100) * 50 + " к зп.");
         }
     }
 }
